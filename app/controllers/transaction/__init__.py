2d238d77ae3b0af7e43052de0c346289ffe1b2db
Wintetth <L.igor.kzn@gmail.com>
2021-06-06 22:01:59 +0300

    Transaction edit function was implemented

--- app/controllers/transaction/__init__.py
+++ app/controllers/transaction/__init__.py
@@ -10,7 +10,7 @@ from app.controllers.transaction.main import view_all_transactions
 transaction: Blueprint = Blueprint(name=__name__.split(".")[-1], import_name=__name__, url_prefix="/transaction/<string:key>")
 
 transaction.add_url_rule(rule="/create", endpoint="create_transaction", view_func=create_transaction, methods=["POST"])
-transaction.add_url_rule(rule="/delete", endpoint="delete_transaction", view_func=delete_transaction, methods=["POST"])
-transaction.add_url_rule(rule="/edit", endpoint="edit_transaction", view_func=edit_transaction, methods=["POST"])
-transaction.add_url_rule(rule="/view", endpoint="view_transaction", view_func=view_transaction, methods=["GET"])
+transaction.add_url_rule(rule="/delete/<int:transaction_id>", endpoint="delete_transaction", view_func=delete_transaction, methods=["POST"])
+transaction.add_url_rule(rule="/edit/<int:transaction_id>", endpoint="edit_transaction", view_func=edit_transaction, methods=["POST"])
+transaction.add_url_rule(rule="/view/<int:transaction_id>", endpoint="view_transaction", view_func=view_transaction, methods=["GET"])
 transaction.add_url_rule(rule="/view_all", endpoint="view_all_transactions", view_func=view_all_transactions, methods=["GET"])
--- app/controllers/transaction/main.py
+++ app/controllers/transaction/main.py
@@ -10,9 +10,10 @@ def create_transaction(key: str):
     if user is None:
         return (jsonify(message="You need to log in before."), 403)
         
-    amount: str = request.form.get("amount", "")
-    if not (amount.isdigit() and int(amount) > 0):
-        return (jsonify(message="Transaction amount should be a digit greater than 0."), 400)
+    try:
+        amount: float = float(request.form.get("amount", ""))
+    except ValueError:
+        return (jsonify(message="Transaction amount should be a digit."), 400)
 
     transaction: Transaction = Transaction.create(user=user, amount=amount)
 
@@ -25,13 +26,37 @@ def create_transaction(key: str):
         201
     )
 
-def delete_transaction(key: str):
+def delete_transaction(key: str, transaction_id: int):
     return (jsonify(message="Transaction deleted."), 201)
 
-def edit_transaction(key: str):
-    return (jsonify(message="Transaction edited."), 201)
+def edit_transaction(key: str, transaction_id: int):
+    user: User = User.get_or_none(key=key)
 
-def view_transaction(key: str):
+    if user is None:
+        return (jsonify(message="You need to log in before."), 403)
+
+    transaction: Transaction = Transaction.get_or_none(transaction_id=transaction_id, user=user)
+    if transaction is None:
+        return (jsonify(message="Transaction was not found."), 404)
+
+    try:
+        amount: float = float(request.form.get("amount", ""))
+    except ValueError:
+        return (jsonify(message="Transaction amount should be a digit."), 400)
+
+    transaction.amount = amount
+    transaction.save()
+
+    return (
+        jsonify(
+            message="Transaction edited.",
+            amount=amount,
+            date=transaction.date.strftime("%Y.%m.%d")
+        ),
+        201
+    )
+
+def view_transaction(key: str, transaction_id: int):
     return (jsonify(message="Transaction viewed."), 200)
 
 def view_all_transactions(key: str):
