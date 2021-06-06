e4a98e39e871c14204e6a72c7c540350bfed2ae5
Wintetth <L.igor.kzn@gmail.com>
2021-06-06 21:30:37 +0300

    Transaction create function was implemented

--- app/controllers/transaction/__init__.py
+++ app/controllers/transaction/__init__.py
@@ -7,7 +7,7 @@ from app.controllers.transaction.main import view_transaction
 from app.controllers.transaction.main import view_all_transactions
 
 
-transaction: Blueprint = Blueprint(name=__name__.split(".")[-1], import_name=__name__, url_prefix="/transaction")
+transaction: Blueprint = Blueprint(name=__name__.split(".")[-1], import_name=__name__, url_prefix="/transaction/<string:key>")
 
 transaction.add_url_rule(rule="/create", endpoint="create_transaction", view_func=create_transaction, methods=["POST"])
 transaction.add_url_rule(rule="/delete", endpoint="delete_transaction", view_func=delete_transaction, methods=["POST"])
--- app/controllers/transaction/main.py
+++ app/controllers/transaction/main.py
@@ -1,18 +1,39 @@
-from flask import jsonify
+from flask import jsonify, request
+
+from app.models.user import User
+from app.models.transaction import Transaction
 
 
-def create_transaction():
-    return (jsonify(message="Transaction created."), 201)
+def create_transaction(key: str):
+    user: User = User.get_or_none(key=key)
 
-def delete_transaction():
+    if user is None:
+        return (jsonify(message="You need to log in before."), 403)
+        
+    amount: str = request.form.get("amount", "")
+    if not (amount.isdigit() and int(amount) > 0):
+        return (jsonify(message="Transaction amount should be a digit greater than 0."), 400)
+
+    transaction: Transaction = Transaction.create(user=user, amount=amount)
+
+    return (
+        jsonify(
+            message="Transaction created.",
+            amount=amount,
+            date=transaction.date.strftime("%Y.%m.%d")
+        ),
+        201
+    )
+
+def delete_transaction(key: str):
     return (jsonify(message="Transaction deleted."), 201)
 
-def edit_transaction():
+def edit_transaction(key: str):
     return (jsonify(message="Transaction edited."), 201)
 
-def view_transaction():
+def view_transaction(key: str):
     return (jsonify(message="Transaction viewed."), 200)
 
-def view_all_transactions():
+def view_all_transactions(key: str):
     return (jsonify(message="All transactions viewed."), 200)
     
